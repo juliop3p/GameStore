a003a8221b6f03c464627573b094f1922af1a1bf
Julio Cesar <julio15.zn@gmail.com>
2022-05-19 23:47:38 -0300

    third lesson

--- Play.Catalog/src/Play.Catalog.Service/Program.cs
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -1,8 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// MongoDb store the data just like it was created so its a string
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
